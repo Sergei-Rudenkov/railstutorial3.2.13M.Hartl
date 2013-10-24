325d280534e0c9315c9366faa40def195896ab83
iResNovae <iresnovaeserg@agmail.com>
2013-10-24 13:11:53 +0300

    Initial commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,4 +37,16 @@ describe "StaticPages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+    describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit static_pages_contact_path
+      page.should have_content('Contact')
+    end
+     it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
