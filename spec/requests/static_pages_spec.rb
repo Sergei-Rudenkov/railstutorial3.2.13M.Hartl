145b6628233c151628e6a87486fe0f9980db15ec
iResNovae <iresnovaeserg@agmail.com>
2013-10-28 01:39:22 +0300

    Initial commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -54,4 +54,17 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
     it { should have_selector 'title', text: '| Contact' }
   end
+    describe "full_title" do
+    it "should include the page title" do
+      full_title("foo").should =~ /foo/
+    end
+
+    it "should include the base title" do
+      full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
+    end
+
+    it "should not include a bar for the home page" do
+      full_title("").should_not =~ /\|/
+    end
+  end
 end
