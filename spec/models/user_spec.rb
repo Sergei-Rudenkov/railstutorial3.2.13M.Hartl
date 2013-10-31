5a548b43bff216211c9cd1458be55ec9c85c862d
iResNovae <iresnovaeserg@agmail.com>
2013-10-31 22:17:51 +0300

    Finish user singup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -101,5 +101,31 @@ describe "with a password that's too short" do
   before { @user.password = @user.password_confirmation = "a" * 5 }
   it { should be_invalid }
 end
+
+describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
   
 end
